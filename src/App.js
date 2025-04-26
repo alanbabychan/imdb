e5758f951a51cfc76262a72158a9c0c3a1bd58c9
alanbabychan <alanbabychan@gmail.com>
2025-04-26 23:40:52 +0530

    updatd app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
 function App() {
-  const data = 'Welcome to my Web App';
   return (
-    <div>
-    <h1 className='hello'>HELLO Kiddo!!</h1>
-    <p>{data}</p>
-    <h3><p>Please like and subscribe to my channel</p></h3>
-    </div>
-    );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
