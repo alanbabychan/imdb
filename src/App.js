a18b20c1f62c3efebdc15cea229fcc73ed8aaeba
alanbabychan <alanbabychan@gmail.com>
2025-04-26 23:01:22 +0530

    basics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  const data = 'Welcome to my Web App';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1 className='hello'>HELLO Kiddo!!</h1>
+    <p>{data}</p>
+    <h3><p>Please like and subscribe to my channel</p></h3>
     </div>
-  );
+    );
 }
 
 export default App;
